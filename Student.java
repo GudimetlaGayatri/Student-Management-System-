a05a3bb07b8f79397ac2af558bda94c997510a53
GudimetlaGayatri <105493236+GudimetlaGayatri@users.noreply.github.com>
2025-06-03 13:08:39 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,76 @@
+package com.example.studentmanagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    @NotBlank
+    private String name;
+
+    @Min(5)
+    @Max(100)
+    private int age;
+
+    @NotBlank
+    private String studentClass;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String address;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
